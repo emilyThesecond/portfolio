b76c99ba4055d64bd810ed991b99671156644bd4
emilyTheseond <esegundo02@gmail.com>
2024-02-22 20:20:48 -0600

    card setup in projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='page' id='projects'>
-      Projects
+        <div className="card">
+                <img src="https://i.imgur.com/aRrgFzu.png" alt="Project 1" />
+                <div className="card-text">
+                <p>Text for Project 1</p>
+                </div>
+        </div>
+        {/* <div className="card">
+                <img src="path_to_your_image.jpg" alt="Project 2" />
+                <div className="card-text">
+                <p>Text for Project 2</p>
+                </div>
+        </div>
+        <div className="card">
+                <img src="path_to_your_image.jpg" alt="Project 3" />
+                <div className="card-text">
+                <p>Text for Project 3</p>
+                </div>
+        </div> */}
     </div>
   )
 }
