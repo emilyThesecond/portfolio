f0848a1fa174189dee4c345fdc3e3207b08e6f7f
emilyTheseond <esegundo02@gmail.com>
2024-04-11 13:29:24 -0500

    create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <h1>Socials</h1>
+       <a href="">LinkedIn</a>
+       <a href="">Github</a>
+       <a href="">Instagram</a>
+
+      </div>
+        <h1>EMILY SEGUNDO</h1>
+        <h2>Chicago, IL</h2>
+        <h2>emily.segundo.n@gmial.com</h2>
+        <h2>(630)640-1286</h2>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
