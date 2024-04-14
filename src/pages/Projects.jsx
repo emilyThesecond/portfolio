df7b8a81c50f5bbffcd2a0789cf4c878c877bd49
emilyTheseond <esegundo02@gmail.com>
2024-04-13 23:29:48 -0500

    before first deployment

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
         {/* <a href="https://the-strokes-fan-page.vercel.app/" target="_blank" rel="noopener noreferrer"> */}
         {/* <button>live site</button> */}
         {/* </a> */}
-        <a href="https://github.com/emilyThesecond/seventy-five-hard" target="_blank" rel="noopener noreferrer"><button>Gitub</button></a>
+        <a href="https://github.com/emilyThesecond/seventy-five-hard" target="_blank" rel="noopener noreferrer"><button>GITHUB</button></a>
         <h3>Experience the ultimate fitness challenge with 75 Hard, the app that keeps you accountable on your wellness journey. Whether you're following the strict 5-rule program or customizing your goals with 75 Soft, journal daily to track your progress and stay committed to your goals. With features like customizable meal plans and progress photo uploads, achieving your fitness ambitions has never been easier. </h3>
         <h4>Technologies Used: Python | Django | SQL </h4>
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.iconfinder.com%2Fdata%2Ficons%2Fconstruction-butterscotch-vol-2%2F512%2FUnder_Construction_2-1024.png&f=1&nofb=1&ipt=76157be84897415ac5dfc92ef122f80d69f4c3c774bdf79f1dfb68ef42ea3793&ipo=images" alt="Under construction image" width = '200'/>
@@ -20,25 +20,25 @@ const Projects = () => {
       <div className="project">
         <h1>The Strokes Fans</h1>
         <a href="https://the-strokes-fan-page.vercel.app/" target="_blank" rel="noopener noreferrer">
-        <button>Live site</button>
+        <button>LIVE SITE</button>
         </a>
-        <a href="https://github.com/emilyThesecond/the-strokes-client" target="_blank" rel="noopener noreferrer"><button>Gitub</button></a>
+        <a href="https://github.com/emilyThesecond/the-strokes-client" target="_blank" rel="noopener noreferrer"><button>GITHUB</button></a>
         <h3>Details: Welcome to the ultimate destination for all things related to The Strokes! 🎸🎶 Dive into the world of this iconic indie rock band and connect with fellow fans. Stay updated on the latest news, tour dates, music releases, and explore their extensive setlists from past performances. Let's celebrate the music and legacy of The Strokes together! This fan page is powered by a full stack MERN (MongoDB, Express.js, React.js, Node.js) application, providing a seamless and interactive experience for fans worldwide.</h3>
         <h4>Technologies Used: MongoDB, Express, React, Node.js</h4>
         <img src="https://i.imgur.com/ks0HzMr.png" alt="Project 1" />
       </div>
       <div className="project">
         <h1>Glaze Cookbook</h1>
-        <a href="https://glaze-cookbook-587f2459d5c3.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button>Live site</button></a>
-        <a href="https://github.com/emilyThesecond/glaze-cookbook" target="_blank" rel="noopener noreferrer"><button>Gitub</button></a>
+        <a href="https://glaze-cookbook-587f2459d5c3.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button>LIVE SITE</button></a>
+        <a href="https://github.com/emilyThesecond/glaze-cookbook" target="_blank" rel="noopener noreferrer"><button>GITHUB</button></a>
         <h3>Details: A full-stack community application for pottery enthusiasts to share real-life experiences, insights on brands, colors, glaze combinations, and techniques, fostering collaboration and support among members.</h3>
         <h4>Technologies Used: MongoDB, Express, Node.js, JavaScript</h4>
         <img src="https://i.imgur.com/s0NrMV0.png" alt="Project 2" />
       </div>
       <div className="project">
         <h1>Habitualize</h1>
-        <a href="https://habitualize-07273966efbf.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button>Live site</button></a>
-        <a href="https://github.com/harmonica23/habitualize" target="_blank" rel="noopener noreferrer"><button>Gitub</button></a>
+        <a href="https://habitualize-07273966efbf.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button>LIVE SITE</button></a>
+        <a href="https://github.com/harmonica23/habitualize" target="_blank" rel="noopener noreferrer"><button>GITHUB</button></a>
         <h3>Details: Habitualize is a Habit Tracker application that helps users build positive habits and break unwanted ones. It provides a user-friendly interface for tracking habits and setting goals. With Habitualize, users can login to their personal tracker, customize their habits and visualize their progress over time, watching their streaks grow as they build or break habits.</h3>
         <h4>Technologies Used: Python, Django, PostgreSQL</h4>
         <img src="https://i.imgur.com/86E64Nb.png" alt="Project 3" />
