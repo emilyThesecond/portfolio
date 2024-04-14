8bcf74669979362f17382fd6efd9c9997615812c
emilyTheseond <esegundo02@gmail.com>
2024-04-13 21:36:59 -0500

    skills page list

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,7 +4,24 @@ const Skills = () => {
   return (
     <div className='page' id='skills'>
       <h1 className='page-header'>S K I L L S</h1>
-      <img src="https://media.istockphoto.com/id/1395006813/vector/foreign-language-translator-at-work-the-concept-of-remote-work.jpg?s=612x612&w=0&k=20&c=ufGfPxYDBMp9TjKi2A1pEkLhrH0uN_UFYWDibGkslL8=" alt="" />
+      <div className='content'>
+        <div className='skills-list'>
+        <ul >
+        <li>Python</li>
+        <li>JavaScript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>SQL</li>
+        <li>MQL</li>
+        <li>JSON</li>
+        <li>EJS</li>
+        <li>DTL</li>
+      </ul>
+
+        </div>
+
+      <img className='skills-pic' src="https://media.istockphoto.com/id/1395006813/vector/foreign-language-translator-at-work-the-concept-of-remote-work.jpg?s=612x612&w=0&k=20&c=ufGfPxYDBMp9TjKi2A1pEkLhrH0uN_UFYWDibGkslL8=" alt="" />
+      </div>
     </div>
   )
 }
